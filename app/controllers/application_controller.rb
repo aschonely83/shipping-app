b9ccb61b7075b7ed7a41420c4719875c3092aac1
Aschonely <aschonely@gmail.com>
2020-01-18 17:28:40 -0500

    update config file and changed the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,12 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
-  register Sinatra::ActiveRecordExtension
-  set :session_secret, "shipping_application_secret"
-  set :views, Proc.new { File.join(root, "../views/") }
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "shipping_security"
+  end
 
   
-
 end
