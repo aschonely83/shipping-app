75af8d246303bbbdf2276e6a42a0db78ea0ee45f
Aschonely <aschonely@gmail.com>
2020-01-18 14:46:00 -0500

    Built my user and retailer controller

--- app/controllers/retailers_controller.rb
+++ app/controllers/retailers_controller.rb
@@ -1,3 +1,46 @@
 class RetailersController < ApplicationController
-    
+  
+  get '/retailers' do
+    @retailers = Retailer.all 
+    erb :'/retailers/index'
+  end
+
+  get 'retailers/new' do
+      erb :'/retailers/new'
+  end
+
+  post '/retailers' do
+    @retailer = Retailer.create(params[:retailer])
+
+    if !params["user"]["name"].empty?
+      @retailer.user = User.create(name: params["user"]["name"])    
+    end
+
+    retailer.save
+
+    redirect to "retailer/#{@retailer.id}"
+  end
+
+  get '/retailers/:id' do
+    @retailer = Retailer.find(params[:id])
+    erb :'/retailers/show'
+  end
+
+  get '/retailers/:id/edit' do
+    @retailer = Retailer.find(params[:id])
+    erb :'/retailers/edit'
+  end
+
+  patch '/retailers/:id' do
+    @retailer = Retailer.find(params[:id])
+    @retailer.update(params[:retailer])
+
+    if !params["user"]["name"].empty?
+      @retailer.user = User.create(name: params["user"]["name"])    
+    end
+
+    retailer.save
+
+    redirect to "retailers/#{@retailer.id}"
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,42 @@
 class UsersController < ApplicationController
  
   get '/users' do
+    @users = User.all
     erb :'/users/index'
   end
 
   get '/users/new' do
+    @retailers = Retailer.all
     erb :'/users/new'
   end
 
   post '/users' do
-    erb :'/users/index'  
+    @user = User.create(params[:user])
+    if !params["retailer"]["name"].empty?
+      @user.retailer << Retailer.create(name: params[:retailer][:name])
+    end
+    redirect "/users/#{@user.id}"  
+  end
+
+  get '/users/:id/edit' do
+    @user = User.find(params[:id])
+    @retailer = Retailer.all
+    erb: '/users/edit'
+  end
+
+  get '/users/:id' do
+    @user = User.find(params[:id])
+    erb :'/users/show'
+  end
+
+  patch '/users/:id' do
+    @user = User.find(params[:id])
+    @user.update(params[:user])
+
+    if !params["retailer"]["name"].empty?
+      @user.retailers << Retailer.create(name: params["retailer"]["name"])
+    end
+
+    redirect "/users/#{@user.id}"
   end
 end
