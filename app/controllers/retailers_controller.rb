7a9e2668e960c3efe5a80f70072027954c0e15ac
Aschonely <aschonely@gmail.com>
2020-01-22 20:47:26 -0500

    reconfigured the view files, updated retailers controller

--- app/controllers/retailers_controller.rb
+++ app/controllers/retailers_controller.rb
@@ -1,24 +1,20 @@
 class RetailersController < ApplicationController
-  #Read
+ 
   #index
   get '/retailers' do
     @retailers = Retailer.all 
     erb :'/retailers/index'
   end
-  
+
   #Create
   #New
   get '/retailers/new' do
     erb :'/retailers/new'
   end
-
-  post '/retailers/new' do
+  
+  post '/retailers' do
     @retailers = Retailer.new(params)
-    if @retailers.save 
-      redirect '/retailers' 
-    else 
-      erb :'/retailers/new'    
-    end
+    redirect '/retailers/#{@retailers[:id]}'    
   end
 
   #show
@@ -33,19 +29,23 @@ class RetailersController < ApplicationController
     erb :'/retailers/edit'
   end
 
-  patch '/retailers/:id' do
-    @retailers = Retailer.find(params[:id])
-    @retailers.name = params[:name]
-    @retailers.boxes = params[:boxes]
-    @retailers.save
-    redirect to '/retailers/#{@retailer.id}'
+  patch "/retailers/:id" do
+    @retailers = Retailer.find_by_id(params[:id])
+    redirect "/retailers" unless @retailers
+    @retailers = Retailer.new(name: params[:name], boxes: params[:boxes], user_id: params[:user_id])
+    if @retailers.update(name: params[:name],boxws: params[:boxes],user_id: params[:user_id])
+    redirect "/retailers/#{@retailers[:id]}"
+    else
+      erb :'/retailers/edit'
+    end  
   end
 
   #delete
-  delete '/retailers/:id' do
+  delete '/retailers/:id/delete' do
     @retailers = Retailer.find(params[:id])
-    @retailers.delete
-    redirect to '/retailers'
+    redirect '/retailers' unless @retailers
+    if @retailers.destroy  
+      redirect '/retailers'
+    end
   end
-
 end
